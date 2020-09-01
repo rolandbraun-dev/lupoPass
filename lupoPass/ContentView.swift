700141ac416a4b0d68ab5a2036fbb7a94766adfa
Roland Braun <mail@rolandbraun.com>
2020-09-01 13:18:48 +0200

    Initial Commit

---
+++ lupoPass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lupoPass
+//
+//  Created by Roland Braun on 9/1/20.
+//  Copyright © 2020 Roland Braun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
